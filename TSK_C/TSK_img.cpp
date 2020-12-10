7f513ac21bff23d078f835d74d4124e91f4145d9
Matthew DeAngelo <mdeangelo@ciphertechsolutions.com>
2020-12-10 11:23:21 -0500

    TSK Commands actually work!

--- TSK_C/TSK_img.cpp
+++ TSK_C/TSK_img.cpp
@@ -2,8 +2,27 @@
 #include "TSK_img.h"
 
 #include <stdio.h>
+#include "tsk/libtsk.h"
 
 void img_open(const char* path)
 {
 	printf(path);
+	char* EO1s[2] = { "C:\\Users\\Matthew\\Desktop\\EO1\\_SOFEX_DOMEX_EX26.2_4GB_MICROSD.E01", "C:\\Users\\Matthew\\Desktop\\EO1\\_SOFEX_DOMEX_EX26.2_4GB_MICROSD.E02" };
+	char* EO1[1] = { "C:\\Users\\Matthew\\Desktop\\EO1\\_SOFEX_DOMEX_EX26.2_4GB_MICROSD.E01" };
+	
+	TSK_IMG_INFO* img = tsk_img_open(1, &EO1, TSK_IMG_TYPE_DETECT, 0);
+	//TSK_IMG_INFO* img = tsk_img_open(2, EO1s, TSK_IMG_TYPE_DETECT, 0);
+	printf(tsk_error_get());
+	printf(tsk_error_get_errstr());
+	printf(tsk_error_get_errstr2());
+
+	TSK_VS_INFO* vs;
+}
+
+
+void getInfo(const char* path) {
+
+	char* EO1s[2] = { "C:\\Users\\Matthew\\Desktop\\EO1\\_SOFEX_DOMEX_EX26.2_4GB_MICROSD.E01", "C:\\Users\\Matthew\\Desktop\\EO1\\_SOFEX_DOMEX_EX26.2_4GB_MICROSD.E02" };
+	TSK_VS_INFO* vs;
+
 }
